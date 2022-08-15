cb28278df34af8e61d44d12c043a27bac276ed02
Karthick Madhesh <karthickmathesh1995@gmail.com>
2022-08-15 23:08:00 +0530

    Initial Commit

---
+++ src/compenents/image/sun.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <svg id="moon" class="h-6 w-6 text-gray-500 cursor-pointer" fill="none" stroke="currentColor" viewbox="0 0 24 24">
+      <path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            stroke-linecap="round" stroke-linejoin="round"
+            stroke-width="2"/>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      component_name: 'Sun Image',
+    }
+  },
+}
+</script>
